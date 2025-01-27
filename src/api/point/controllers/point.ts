927710fb051f80d64744ac41dd27dfdc7656ac9b
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2025-01-27 14:09:15 -0300

    progress interactive map

---
+++ src/api/point/controllers/point.ts
@@ -0,0 +1,7 @@
+/**
+ * point controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::point.point');
---
+++ src/api/points-category/controllers/points-category.ts
@@ -0,0 +1,7 @@
+/**
+ * points-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::points-category.points-category');
