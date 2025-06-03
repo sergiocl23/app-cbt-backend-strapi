63d630ae95a3f356b961e97bfaa03add8d81dae1
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2025-06-03 13:50:45 -0400

    Change mailing service

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,6 +6,7 @@ export default () => ({
       defaultDepth: 3
     }
   },
+  /*
   email: {
     config: {
       provider: 'nodemailer',
@@ -21,6 +22,24 @@ export default () => ({
       settings: {
         defaultFrom: env('SMTP_FROM', 'newsletter@corredorbioceanico.com'),
         defaultReplyTo: env('SMTP_REPLY_TO', 'soporte@corredorbioceanico.com'),
+      },
+    },
+  },
+  */
+ email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'localhost',
+        port: 25,
+        secure: false, // No usar SSL si usas postfix local
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      settings: {
+        defaultFrom: '"Proyecto Corredor Bioceánico" <proyectocb@corredor-bioceanico-tarapaca.cl>',
+        defaultReplyTo: 'proyectocb@corredor-bioceanico-tarapaca.cl',
       },
     },
   },
