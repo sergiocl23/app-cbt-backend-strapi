3edf72ee2b44d3b5c07acc40159b8b63ec8221b3
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2025-07-24 12:58:08 -0400

    try 2 fix validation email redirection

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,51 +6,14 @@ export default () => ({
       defaultDepth: 3
     }
   },
-  /*
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.gmail.com'),
-        port: env.int('SMTP_PORT', 465),
-        secure: true,
-        auth: {
-          user: env('SMTP_USER'),
-          pass: env('SMTP_PASSWORD'),
-        },
-      },
-      settings: {
-        defaultFrom: env('SMTP_FROM', 'newsletter@corredorbioceanico.com'),
-        defaultReplyTo: env('SMTP_REPLY_TO', 'soporte@corredorbioceanico.com'),
-      },
-    },
-  },
-  */
-//  email: {
-//     config: {
-//       provider: 'nodemailer',
-//       providerOptions: {
-//         host: 'localhost',
-//         port: 25,
-//         secure: false, // No usar SSL si usas postfix local
-//         tls: {
-//           rejectUnauthorized: false,
-//         },
-//       },
-//       settings: {
-//         defaultFrom: '"Proyecto Corredor Bioceánico" <proyectocb@corredor-bioceanico-tarapaca.cl>',
-//         defaultReplyTo: 'proyectocb@corredor-bioceanico-tarapaca.cl',
-//       },
-//     },
-//   },
   'users-permissions': {
     config: {
       register: {
-        emailConfirmation: true, // activa confirmación por email
+        emailConfirmation: true,
       },
       email: {
         confirmation: {
-          url: `${env('SERVER_URL')}/api/auth/email-confirmation`,
+          url: `${env('PUBLIC_URL')}/api/auth/email-confirmation`, // ← usa dominio público
         },
       },
     },
@@ -61,7 +24,7 @@ export default () => ({
       providerOptions: {
         sendmail: true,
         newline: 'unix',
-        path: '/usr/sbin/sendmail', // o '/usr/sbin/sendmail' según tu sistema
+        path: '/usr/sbin/sendmail',
       },
       settings: {
         defaultFrom: '"Proyecto Corredor Bioceánico" <proyectocb@corredor-bioceanico-tarapaca.cl>',
@@ -69,13 +32,11 @@ export default () => ({
       },
     },
   },
-
-
   upload: {
     config: {
       provider: 'local',
       providerOptions: {},
-      sizeLimit: 20 * 1024 * 1024, // 20 MB en bytes
+      sizeLimit: 20 * 1024 * 1024,
     },
   },
 });
