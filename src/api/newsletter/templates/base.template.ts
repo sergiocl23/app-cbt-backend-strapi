fef1ab3f1a6ac78e3f1c7f24933d951840449b47
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2025-08-08 01:14:20 -0400

    add isActive column on topic collection

--- src/api/newsletter/templates/base.template.ts
+++ src/api/newsletter/templates/base.template.ts
@@ -73,7 +73,7 @@ export const renderNewsletter = (noticiasPorPais: NoticiasPorPais, type: string,
 
   const template = `
     <!DOCTYPE html>
-    <html>
+    <html lang="es">
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
